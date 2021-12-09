47881b8e7274c999c7ca63d5865fb90266c480d7
Theo McArn <theo_mcarn@brown.edu>
2021-12-09 17:32:42 -0500

    skjv

--- code/models.py
+++ code/models.py
@@ -64,10 +64,10 @@ class YourModel(tf.keras.Model):
               # MaxPool2D(2, name="block1_pool"),
 
               Conv2D(64, 3, 1, padding="same",
-                    activation="relu", name="block2_conv1"),
+                    activation="relu", name="block1_conv1"),
               Conv2D(64, 3, 1, padding="same",
-                    activation="relu", name="block2_conv2"),
-              MaxPool2D(2, name="block2_pool"),
+                    activation="relu", name="block1conv2"),
+              MaxPool2D(2, name="block1_pool"),
 
               # Conv2D(128, 3, 1, padding="same",
               #       activation="relu", name="block3_conv1"),
