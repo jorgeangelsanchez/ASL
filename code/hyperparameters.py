4ea8a10ae00675b7baa4574120a04090e9a88e65
Theo McArn <theo_mcarn@brown.edu>
2021-12-09 18:44:51 -0500

    skjv

--- code/hyperparameters.py
+++ code/hyperparameters.py
@@ -9,14 +9,14 @@ Number of epochs. If you experiment with more complex networks you
 might need to increase this. Likewise if you add regularization that
 slows training.
 """
-num_epochs = 50
+num_epochs = 70
 
 """
 A critical parameter that can dramatically affect whether training
 succeeds or fails. The value for this depends significantly on which
 optimizer is used. Refer to the default learning rate parameter
 """
-learning_rate = 3e-4
+learning_rate = 5e-4
 
 """
 Momentum on the gradient (if you use a momentum-based optimizer)
