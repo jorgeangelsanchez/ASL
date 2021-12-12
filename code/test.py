b8a00fba3d7d27080268c02f8ce592e9afc3e53f
Theo McArn <theo_mcarn@brown.edu>
2021-12-12 16:24:25 -0500

    a

--- code/test.py
+++ code/test.py
@@ -107,7 +107,7 @@ plt.show()
 alphadict = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J', 10:'K', 11:'L', 12:'M',
                  13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U', 21:'V', 22:'W', 23:'X', 24:'Y'}
 
-predicted_classes = cnn_model.predict_classes(X_test)
+predicted_classes = np.argmax(cnn_model.predict(X_test), axis=-1)
 
 L = 5
 W = 5
@@ -174,7 +174,7 @@ np.set_printoptions(precision=2)
 
 #Specifying class labels
 #Specifying class labels
-class_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+class_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
                  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y' ]
 
 plt.figure(figsize=(20,20))
