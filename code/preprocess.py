6212cf6aecee38a0dcc88f314d4600652dfb7dd6
jorgeangelsanchez <jorge_sanchez@brown.edu>
2021-12-14 17:10:45 -0500

    last

--- code/preprocess.py
+++ code/preprocess.py
@@ -42,7 +42,6 @@ class Datasets():
         self.test_data = self.get_data(
             os.path.join(self.data_path, "test/"), task == '3', False, False)
         
-
     def calc_mean_and_std(self):
         """ Calculate mean and standard deviation of a sample of the
         training dataset for standardization.
